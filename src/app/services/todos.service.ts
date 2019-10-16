3c59efbfea81f023f7cbfb4f0f7c02b1915ec1e3
NaveenKumar7845 <naveenbtechit2015@gmail.com>
2019-10-16 17:00:59 +0530

    2 files modified

--- src/app/services/todos.service.ts
+++ src/app/services/todos.service.ts
@@ -1,9 +1,30 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TodosService {
 
-  constructor() { }
+    private baseURL="https://jsonplaceholder.typicode.com/todos";
+
+
+  constructor(private http:HttpClient) {
+     console.log("Todo Service created....")
+   }
+
+
+getAllTodos():Observable<any>{
+ return this.http.get(this.baseURL)
+}
+
+getTodosId(todosId:number):Observable<any>{
+  return this.http.get(this.baseURL+"/"+todosId);
+ }
+
+ 
+ getTodosByUserId(userId:number):Observable<any>{
+  return this.http.get(this.baseURL+"?userId="+userId);
+ }
 }
--- src/app/todos/todos.component.ts
+++ src/app/todos/todos.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { PostsService } from '../services/posts.service';
+import { ActivatedRoute } from '@angular/router';
+import { PagerService } from '../services/pager.service';
+import { StudentDetailsService } from './services/todos.service';
 
 @Component({
   selector: 'app-todos',
@@ -6,7 +10,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./todos.component.css']
 })
 export class TodosComponent implements OnInit {
+  title='All Posts';
 
+  todos:any;
   constructor() { }
 
   ngOnInit() {
